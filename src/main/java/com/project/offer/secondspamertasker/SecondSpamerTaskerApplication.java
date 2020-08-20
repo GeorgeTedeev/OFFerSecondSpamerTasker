b6136945ad46e87bf4db62553ca0f05fe9a00ce8
GeorgeTedeev <biruang123@gmail.com>
2020-08-20 11:09:11 +0300

    init

---
+++ src/main/java/com/project/offer/secondspamertasker/SecondSpamerTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.project.offer.secondspamertasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpamerTaskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSpamerTaskerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/offer/secondspamertasker/SecondSpamerTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.offer.secondspamertasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpamerTaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
