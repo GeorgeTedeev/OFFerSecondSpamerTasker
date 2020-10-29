b0f319144486cb71443c280234fdbe4e646864e0
George Tedeev <georgetd@mail.ru>
2020-10-29 19:09:34 +0300

    solved deserialization problem

--- src/main/java/com/project/offer/secondspamertasker/configs/KafkaConfig.java
+++ src/main/java/com/project/offer/secondspamertasker/configs/KafkaConfig.java
@@ -1,6 +1,8 @@
 package com.project.offer.secondspamertasker.configs;
 
+
 import com.project.offer.secondspamertasker.entities.SpamTask;
+import com.project.offer.secondspamertasker.entities.User;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +11,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -20,20 +21,38 @@ import java.util.Map;
 public class KafkaConfig {
 
     @Bean
-    public ConsumerFactory<String, SpamTask> kafkaConsumerFactory() {
+    public ConsumerFactory<String, SpamTask> kafkaConsumerFactoryForSpamTask() {
         JsonDeserializer<SpamTask> deserializer = new JsonDeserializer<>(SpamTask.class, false);
         deserializer.addTrustedPackages("*");
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         return new DefaultKafkaConsumerFactory<>(props,new StringDeserializer(), deserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, SpamTask> kafkaListenerContainerFactory(){
+    public ConcurrentKafkaListenerContainerFactory<String, SpamTask> kafkaListenerContainerFactoryForSpamTask(){
         ConcurrentKafkaListenerContainerFactory<String, SpamTask> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(kafkaConsumerFactory());
+        factory.setConsumerFactory(kafkaConsumerFactoryForSpamTask());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, User> kafkaConsumerFactoryForUser() {
+        JsonDeserializer<User> deserializer = new JsonDeserializer<>(User.class, false);
+        deserializer.addTrustedPackages("*");
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+        return new DefaultKafkaConsumerFactory<>(props,new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, User> kafkaListenerContainerFactoryForUser(){
+        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(kafkaConsumerFactoryForUser());
         return factory;
     }
 
